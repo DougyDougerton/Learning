61d5b8d87e82fa7598832451b68b3ba3ec38ccda
Douglas Spicer <doug@Douglass-MacBook-Air.local>
2024-08-13 21:18:02 -0600

    For the lawlz

---
+++ soHard.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class soHard {
+    public static void main(String[] args) {
+        for(int i = 0; i < 100; i--){
+        System.out.println(i);
+        };
+        String thisClass = "so hard";
+        JOptionPane.showMessageDialog(null, thisClass);
+
+        System.out.println(thisClass);
+    }
+    
+}
